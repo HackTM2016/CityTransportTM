3efa22348fb5dd93363633a6b69c079b637900f4
DenisTDR <denis.tdr@gmail.com>
2016-05-22 07:18:58 +0300

    preparing currentStation logic

---
+++ frontend/www/views/currentStation/currentStation.js
@@ -0,0 +1,12 @@
+/**
+ * Created by NM on 5/21/2016.
+ */
+
+console.log("current controller loading");
+
+appControllers
+
+  .controller('CurrentStationController', function($scope, StationsService, $http) { // uiGmapGoogleMapApi
+    console.log("current station controller loaded");
+
+  });
