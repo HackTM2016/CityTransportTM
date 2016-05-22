e00e77a8ab7663f076301409039d8b59540b0dc7
DenisTDR <denis.tdr@gmail.com>
2016-05-22 06:19:24 +0300

    routeDetail logic

--- frontend/www/views/routeDetails/routeDetails.js
+++ frontend/www/views/routeDetails/routeDetails.js
@@ -5,23 +5,57 @@
 console.log("route details controller loading");
 
 appControllers
-  .controller('RouteDetailsController', function($scope, StationsService, $rootScope, $location) {
+  .controller('RouteDetailsController', function($scope, StationsService, RoutesService, $rootScope, $location) {
     console.log("route details controller loaded");
-
+    $scope.routes = [];
+    $scope.route_id = 0;
+    $scope.route = null;
     $scope.init = function () {
+      $scope.$on('openRouteDetails', function() {
+        $scope.loadRoute($rootScope.selectedRoute);
+      });
+
       if($rootScope.selectedRoute == undefined) {
         $location.path('/view/route');
         return;
       }
-      $scope.$on('openRouteDetails', function() {
-        $scope.loadRoute($rootScope.selectedRoute);
-      });
+
       $scope.loadRoute($rootScope.selectedRoute);
+
+      console.log($rootScope.selectedRoute);
+
     };
 
     $scope.loadRoute = function(route) {
+      $scope.route = route;
       console.log("loading route(details): ", route);
+      RoutesService.getRoute(route.line_id).then(function (data) {
+        console.log("sdfdsf", data);
+        $scope.routes = data.data.routes;
+        $scope.getTimes();
+      });
+
     };
+
+
+    $scope.getTimes = function () {
+      var crtRouteId = $scope.route_id;
+      RoutesService.getTimes($scope.route.line_id, crtRouteId).then(function (data) {
+        console.log("got times");
+        console.log(data);
+        data.data.arrivals.forEach(function (arrival) {
+
+          $scope.routes[crtRouteId].stations.forEach(function (station) {
+            if(station.station_id == arrival.station_id){
+              station.arrivalTime = arrival.arrival;
+              station.minutesLeft = arrival.minutes_left;
+            }
+          });
+        });
+      })
+    };
+
+
 
     $scope.init();
   });
